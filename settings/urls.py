11f0ac0445576b8c83c41c1c812d5d1bf1c41cb5
chenchiyuan <chenchiyuan03@gmail.com>
2013-06-25 17:13:42 +0800

    将api归入到api的url的命名空间下，并授予版本v0，方便以后扩展

--- settings/urls.py
+++ settings/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^webooks/', include('webooks.foo.urls')),
 
     url(r'^', include("webooks.urls.urls")),
+    url(r'^api/v0/', include("webooks.apis.urls")),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
