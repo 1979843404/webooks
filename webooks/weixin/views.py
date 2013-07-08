7382241c430dd774495016352280b2350f0adb3e
chenchiyuan <chenchiyuan03@gmail.com>
2013-07-08 13:50:00 +0800

    测试下微信接口

--- webooks/weixin/views.py
+++ webooks/weixin/views.py
@@ -4,11 +4,13 @@
 from __future__ import division, unicode_literals, print_function
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from webooks.utils.https import json_response
 from weixin import WeiXin
+from webooks.models import Book
+import time
 
 @csrf_exempt
 def interface(request):
+    print(request)
     if request.method == 'GET':
         echostr = request.GET.get("echostr", "")
         return HttpResponse(echostr)
@@ -16,5 +18,17 @@ def interface(request):
         content = request.body
         wx = WeiXin.on_message(content)
         json_data = wx.to_json()
-        print(json_data)
-        return json_response(json_data)
+        content = json_data.get("Content", "")
+        books = Book.objects.all().filter(name__icontains=content).values_list('name')
+        book_contents = [book[0] for book in books]
+        book_content = "<br>".join(book_contents)
+        to_user_name = json_data.get("FromUserName", "")
+        from_user_name = json_data.get("ToUserName", "")
+        create_time = time.time()
+        msg_type = "text"
+        content = book_content
+        func_flag = 0
+        xml_data = wx.to_xml(to_user_name=to_user_name, from_user_name=from_user_name, create_time=create_time,
+            msg_type=msg_type, content=content, func_flag=func_flag)
+
+        return HttpResponse(xml_data)
