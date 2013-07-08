c7ec4f6d4cce82630a4612b948932aadcdf72985
chenchiyuan <chenchiyuan03@gmail.com>
2013-07-08 13:35:21 +0800

    增加了weixin接口

--- webooks/weixin/views.py
+++ webooks/weixin/views.py
@@ -3,10 +3,16 @@
 
 from __future__ import division, unicode_literals, print_function
 from django.http import HttpResponse
+from webooks.utils.https import json_response
+from weixin import WeiXin
 
 def interface(request):
     if request.method == 'GET':
         echostr = request.GET.get("echostr", "")
+        return HttpResponse(echostr)
     else:
-        echostr = ""
-    return HttpResponse(echostr)
+        content = request.body
+        wx = WeiXin.on_message(content)
+        json_data = wx.to_json()
+        print(json_data)
+        return json_response(json_data)
